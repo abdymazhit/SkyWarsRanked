b6c3087a89c2ea0e9c344b4b38cebf25eb690f0c
Islam Abdymazhit <islam.abdymazhit@gmail.com>
2021-08-03 10:58:34 +0300

    Добавлено событие выхода игрока из сервера

---
+++ src/main/java/net/Abdymazhit/SkyWarsRanked/events/PlayerQuitListener.java
@@ -0,0 +1,75 @@
+package net.Abdymazhit.SkyWarsRanked.events;
+
+import net.Abdymazhit.SkyWarsRanked.Config;
+import net.Abdymazhit.SkyWarsRanked.SkyWarsRanked;
+import net.Abdymazhit.SkyWarsRanked.customs.Island;
+import net.Abdymazhit.SkyWarsRanked.managers.GameStage;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ * Отвечает за событие выхода игрока из сервера
+ *
+ * @version   03.08.2021
+ * @author    Islam Abdymazhit
+ */
+public class PlayerQuitListener implements Listener {
+
+    /**
+     * Событие выхода игрока из сервера
+     */
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        // Проверка стадии игры на WAITING или STARTING
+        if(SkyWarsRanked.getGameManager().getGameStage().equals(GameStage.WAITING) || SkyWarsRanked.getGameManager().getGameStage().equals(GameStage.STARTING)) {
+            // Проверка, является ли игрок игроком игры
+            if(SkyWarsRanked.getGameManager().getPlayers().contains(player)) {
+                // Очистить игрока из острова
+                for(Island island : Config.islands) {
+                    if(island.getPlayer() != null) {
+                        if(island.getPlayer().equals(player)) {
+                            island.setPlayer(null);
+                        }
+                    }
+                }
+
+                // Удалить игрока из списка игроков игры
+                SkyWarsRanked.getGameManager().removePlayer(player);
+                event.setQuitMessage("[" + SkyWarsRanked.getGameManager().getPlayers().size() + "/" + Config.islands.size() + "] " +
+                        "§e=> §fИгрок " + player.getDisplayName() + " отключился");
+            }
+            // Проверка, является ли игрок зрителем игры
+            else if(SkyWarsRanked.getGameManager().getSpectators().contains(player)) {
+                // Удалить игрока из списка зрителей игры
+                event.setQuitMessage(null);
+                SkyWarsRanked.getGameManager().removeSpectator(player);
+            }
+        }
+        // Проверка стадии игры на GAME
+        else if(SkyWarsRanked.getGameManager().getGameStage().equals(GameStage.GAME)) {
+            // Проверка, является ли игрок игроком игры
+            if (SkyWarsRanked.getGameManager().getPlayers().contains(player)) {
+                // Удалить игрока из списка игроков игры
+                SkyWarsRanked.getGameManager().removePlayer(player);
+                event.setQuitMessage("Игрок " + player.getDisplayName() + " вышел из игры и тем самым был самоубит");
+            }
+            // Проверка, является ли игрок зрителем игры
+            else if(SkyWarsRanked.getGameManager().getSpectators().contains(player)) {
+                // Удалить игрока из списка зрителей игры
+                event.setQuitMessage(null);
+                SkyWarsRanked.getGameManager().removeSpectator(player);
+            }
+        }
+        // Проверка стадии игры на ENDING
+        else if(SkyWarsRanked.getGameManager().getGameStage().equals(GameStage.ENDING)) {
+            // Удалить игрока из списка игроков и зрителей игры
+            event.setQuitMessage(null);
+            SkyWarsRanked.getGameManager().removePlayer(player);
+            SkyWarsRanked.getGameManager().removeSpectator(player);
+        }
+    }
+}
